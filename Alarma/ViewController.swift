5cf5d79c54c882cc4821a62e47adbb4e21e73cf5
Andre Fiebig <Andre@andres-imac.local>
2016-08-05 01:22:35 +0200

    Initial Commit

---
+++ Alarma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alarma
+//
+//  Created by Andre Fiebig on 05.08.16.
+//  Copyright © 2016 Andre Fiebig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
